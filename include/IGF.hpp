60fd03240796b639a6f44285713cdca2c8c7d917
Adam Flett <adam@adamflett.net>
2023-12-01 06:30:38 +0800

    new setup

---
+++ include/IGF.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace igf
+{
+    class IGF
+    {
+    public:
+        IGF();
+        ~IGF();
+
+        void PrintTest();
+
+    private:
+        bool m_running;
+    };
+}
---
+++ src/IGF.cpp
@@ -0,0 +1,20 @@
+#include "IGF.hpp"
+
+#include <iostream>
+
+namespace igf
+{
+    IGF::IGF()
+    {
+    }
+
+    IGF::~IGF()
+    {
+    }
+
+    void IGF::PrintTest()
+    {
+        std::cout << "Testing from IGF.cpp"
+                  << "\n";
+    }
+}
