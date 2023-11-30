b294d31afcff55b314e97ca852831b3ecceb892e
Adam Flett <adam@adamflett.net>
2023-12-01 07:34:27 +0800

    trying to static link SDL into the  lib

--- src/IGF.cpp
+++ src/IGF.cpp
@@ -1,15 +1,53 @@
 #include "IGF.hpp"
 
 #include <iostream>
+#include <SDL3/SDL.h>
 
 namespace igf
 {
     IGF::IGF()
     {
-    }
+
+        SDL_Init(SDL_INIT_EVERYTHING);
+
+        SDL_Window *window =
+            SDL_CreateWindow("SDLVulk Test", 800, 600, SDL_WindowFlags::SDL_WINDOW_RESIZABLE);
+
+        SDL_Renderer *renderer = SDL_CreateRenderer(
+            window, NULL, SDL_RendererFlags::SDL_RENDERER_ACCELERATED);
+
+        SDL_Event event;
+        bool m_running = true;
+        while (m_running)
+        {
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_EVENT_QUIT)
+                {
+                    m_running = false;
+                }
+                if (event.type == SDL_EVENT_KEY_DOWN)
+                {
+                    auto keysympressed = event.key.keysym.sym;
+                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                    {
+                        m_running = false;
+                    }
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
+        }
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        }
 
     IGF::~IGF()
     {
+        SDL_Quit();
     }
 
     void IGF::PrintTest()
